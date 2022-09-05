b3e72d276b457b42df97d614245bdf72feb6df17
kchaitanya98 <56799589+kchaitanya98@users.noreply.github.com>
2022-09-05 11:41:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const index=require('./index');
-const connection=require('./dbconnection');
-const mongoConnection=require('./dbconnectionmongo');
-//const checkAuth = require('./middleware/check-auth');
+// const connection=require('./dbconnection');
+// const mongoConnection=require('./dbconnectionmongo');
+// //const checkAuth = require('./middleware/check-auth');
 
-if(connection&&index){
-    console.log('connection, index file valid');
-    console.log(mongoConnection);
-}
-
-
+// if(connection&&index){
+//     console.log('connection, index file valid');
+//     console.log(mongoConnection);
+// }
+console.log(index);
